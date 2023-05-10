657974a248de5b8829b4f30d229d387551566879
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-10 12:59:41 -0500

    completed labwork

---
+++ lib/migrations/versions/a9828d40adaf_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: a9828d40adaf
+Revises: 55214a38afc8
+Create Date: 2023-05-10 12:20:46.683722
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a9828d40adaf'
+down_revision = '55214a38afc8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ lib/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from sqlalchemy import create_engine, desc
+from sqlalchemy import Column, DateTime, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///migrations_test.db')
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), index=True)
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
